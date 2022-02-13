235edb247bdfb27050f75d7388a9bc279d39dd5f
sanskar8176 <sanskarsingh8176@gmail.com>
2022-02-13 19:28:44 +0530

    created portfolio

---
+++ src/Components/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map(function (network) {
+      return (
+        <li key={network.name} >
+          <a href={network.url} target="_blank" rel="noreferrer">
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links" >{networks}</ul>
+
+          <ul className="copyright">
+            <li>
+              Code by{" "}
+              <a title="Sanskar Singh" href="https://linkedin.com/in/sanskar-singh-98ba16193" target="_blank" rel="noreferrer">
+              Sanskar Singh 
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
